76e6f6856aa7765cc7d096ebabe25eddf7143e33
aliashkov <art1699584@gmail.com>
2024-10-28 11:20:28 +0300

    main | fix: styles on background

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -116,7 +116,7 @@ const Contact = () => {
             <div className="map-container">
                 <iframe
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d587.5972068757675!2d27.600075899999998!3d53.939536099999995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46dbcf1521b04a37%3A0xabbad91a50ddbdee!2sOf.%20413%2C%20vulica%20Mielie%C5%BEa%201%2C%20Minsk%2C%20Minsk%20Region!5e0!3m2!1sen!2sby!4v1698500123456!5m2!1sen!2sby"
-                    width="100%"
+                    width="500"
                     height="100%"
                     style={{ border: 0 }}
                     allowFullScreen=""
